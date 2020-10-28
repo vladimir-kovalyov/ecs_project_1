06d670af698a368ce45c1b714c24b19b7e60ff29
Vladimirs Kovalovs <vladimirs.kovalovs@ecs-digital.co.uk>
2020-10-28 16:21:28 +0000

    Added file size and extension validation for shell script

--- assets/blogs/getblogs.sh
+++ assets/blogs/getblogs.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 input=./links.txt
 o=blogfiles/
 
 cd $o
-while IFS= read -r line
+
+myarr=($(awk -F':' '{ print $0 }' "../$input" | sed -ne '/.txt$/p' -e '/.doc$/p' -e '/.md$/p'))
+
+for i in "${myarr[@]}"
 do
-  curl $line -O 
-done < "../$input"
+  curl --max-filesize 1m $i -O
+done
