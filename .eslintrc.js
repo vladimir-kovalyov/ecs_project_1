21bfa997d468336210c525db3ad96b9c2d0ba5bb
Vladimirs Kovalovs <vladimirs.kovalovs@ecs-digital.co.uk>
2020-10-29 11:29:00 +0000

    Added ESLint v.1

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'rules': {
+    "no-console": 0,
+    "no-empty": 0,
+    "no-irregular-whitespace":0,
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
+  
+  },
+};
