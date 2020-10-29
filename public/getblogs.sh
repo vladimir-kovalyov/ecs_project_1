82f5dcc8e6072c7880585387c9b2019218c44bb8
Michael Skidmore <Michael.Skidmore@ecs-digital.co.uk>
2020-10-29 15:29:07 +0000

    Moving scripts

---
+++ public/getblogs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+input=./links.txt
+o=blogfiles/
+
+cd $o
+
+myarr=($(awk -F':' '{ print $0 }' "../$input" | sed -ne '/.txt$/p' -e '/.doc$/p' -e '/.md$/p'))
+
+for i in "${myarr[@]}"
+do
+  curl --max-filesize 1m $i -O
+done
