bb45ec8d2278ef582a253a640963fcd3170e8336
Michael Skidmore <Michael.Skidmore@ecs-digital.co.uk>
2020-10-30 12:37:16 +0000

    Final commit, still missing some stuff

---
+++ app.test.js
@@ -0,0 +1,10 @@
+/*
+
+const dbOps = require('./app.js');
+
+test('Get blog data should not be undefined', async () => {
+  const data = await getBlogs();
+  expect(data).not().toBeUndefined();
+});
+
+*/
